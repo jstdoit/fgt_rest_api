599269b7c8a46f4454d92300e28c558115101f32
Stefan Feurle <stefan.feurle@gmail.com>
2017-12-20 02:31:31 +0100

    replaced gsub with tr

--- lib/fgt_rest_api/ipsec.rb
+++ lib/fgt_rest_api/ipsec.rb
@@ -4,7 +4,7 @@ module FGT
     #vpn ipsec
     %w[phase1 phase1_interface phase2 phase2_interface forticlient].each do |name|
       define_method('vpn_ipsec_' + name) do |vdom = use_vdom|
-        cmdb_get(path: 'vpn.ipsec', name: name.gsub('_', '-'), vdom: vdom)
+        cmdb_get(path: 'vpn.ipsec', name: name.tr('_', '-'), vdom: vdom)
       end
     end
 
