d4e6b435a86f59090985d6c4267e459c8e5b80fa
Stefan Feurle <stefan.feurle@gmail.com>
2017-12-29 12:17:01 +0100

    cleanup

--- lib/fgt_rest_api/fc_hash.rb
+++ lib/fgt_rest_api/fc_hash.rb
@@ -15,15 +15,13 @@ module FGT
     end
 
     def [](key)
+      raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key.to_s[0] == key.to_s[0].downcase
       if key.is_a?(String)
-        raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key[0] == key[0].downcase
         super(key)
       elsif key.is_a?(Symbol)
-        raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key.to_s[0] == key.to_s[0].downcase
-        key = 'secondary-IP' if key == :secondary_ip
         super(key.to_s) || super(key.to_s.tr('_', '-'))
       else
-        raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key.is_a?(String)
+        raise(ArgumentError, "'key needs to be a string and key needs start with downcase: >>#{key.inspect}<<") unless key.is_a?(String)
       end
     end
 
