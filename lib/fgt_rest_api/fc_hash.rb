5d55f71236ab7d8dc092ac9a3762a4d12c39f12e
Stefan Feurle <stefan.feurle@gmail.com>
2017-12-29 12:13:30 +0100

    fixed a bug in fc_hash
    keys (symbol and string) and methods can contain upper case letters now; only the first letter has to be lower case.

--- lib/fgt_rest_api/fc_hash.rb
+++ lib/fgt_rest_api/fc_hash.rb
@@ -4,7 +4,7 @@ module FGT
 
     def []=(key, value)
       raise(ArgumentError, 'key needs to be a string') unless key.is_a?(String)
-      raise(ArgumentError, 'key needs to be downcase') unless key == key.downcase
+      raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key[0] == key[0].downcase
       define_singleton_method(key.tr('-', '_')) do
         value
       end
@@ -16,13 +16,14 @@ module FGT
 
     def [](key)
       if key.is_a?(String)
-        raise(ArgumentError, 'key needs to be downcase') unless key == key.downcase
+        raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key[0] == key[0].downcase
         super(key)
       elsif key.is_a?(Symbol)
-        raise(ArgumentError, 'key needs to be downcase') unless key.to_s == key.to_s.downcase
+        raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key.to_s[0] == key.to_s[0].downcase
+        key = 'secondary-IP' if key == :secondary_ip
         super(key.to_s) || super(key.to_s.tr('_', '-'))
       else
-        raise(ArgumentError, 'key needs to be a downcase string or a symbol') unless key.is_a?(String)
+        raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key.is_a?(String)
       end
     end
 
