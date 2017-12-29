b27efcc44c1893e36d24e07895dafc92af51ea23
Stefan Feurle <stefan.feurle@gmail.com>
2017-12-29 13:29:50 +0100

    fixed logical bug & cleanup

--- lib/fgt_rest_api/fc_hash.rb
+++ lib/fgt_rest_api/fc_hash.rb
@@ -2,26 +2,32 @@ module FGT
 
   class FCHash < ::Hash
 
+    def attribute_accessors(key, value)
+      method_name = key.to_s.tr('-', '_')
+      return nil if respond_to?(method_name.to_sym)
+      define_singleton_method(method_name) { value }
+      define_singleton_method("#{method_name}=") { |value| self[key] = value }
+    end
+
     def []=(key, value)
-      raise(ArgumentError, 'key needs to be a string') unless key.is_a?(String)
-      raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key[0] == key[0].downcase
-      define_singleton_method(key.tr('-', '_')) do
-        value
+      raise(ArgumentError, "key needs start with downcase letter: >>#{key}<<") unless key[0] == key[0].downcase
+      attribute_accessors(key, value)
+      if key.is_a?(String)
+        super(key, value)
+      elsif key.is_a?(Symbol)
+        super(key.to_s.tr('_', '-'), value)
+      else
+        raise(ArgumentError, "'key needs to be a string and key needs start with downcase letter: >>#{key.inspect}<<") unless key.is_a?(String)
       end
-      define_singleton_method(key.tr('-', '_') + '=') do |value|
-        self[key] = value
-      end
-      super(key, value)
     end
 
     def [](key)
       raise(ArgumentError, "key needs start with downcase: >>#{key}<<") unless key.to_s[0] == key.to_s[0].downcase
-      if key.is_a?(String)
         super(key)
       elsif key.is_a?(Symbol)
         super(key.to_s) || super(key.to_s.tr('_', '-'))
       else
-        raise(ArgumentError, "'key needs to be a string and key needs start with downcase: >>#{key.inspect}<<") unless key.is_a?(String)
+        raise(ArgumentError, "'key needs to be a string and key needs start with downcase letter: >>#{key.inspect}<<") unless key.is_a?(String)
       end
     end
 
