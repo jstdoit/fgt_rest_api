47da2d2c1564afd44326f2ca3c31f7750e6b9d0b
Stefan Feurle <stefan.feurle@gmail.com>
2017-12-30 16:27:48 +0100

    refactoring and cleanup attribute_methods method

--- lib/fgt_rest_api/fc_hash.rb
+++ lib/fgt_rest_api/fc_hash.rb
@@ -28,11 +28,22 @@ module FGT
     private
 
     def attribute_methods(key, value)
+      getter_method(key)
+      setter_method(key, value)
       method_name = key.to_s.tr('-', '_')
-      return value if respond_to?(method_name.to_sym)
-      define_singleton_method(method_name) { fetch(key) }
-      define_singleton_method("#{method_name}=") { |v| store(key, v) }
       value
+    end
+
+    def getter_method(key)
+      method_name = key.to_s.tr('-', '_')
+      return true if respond_to?(method_name.to_sym)
+      define_singleton_method(method_name) { fetch(key) }
+    end
+
+    def setter_method(key, value)
+      method_name = key.to_s.tr('-', '_') + '='
+      return true if respond_to?(method_name.to_sym)
+      define_singleton_method("#{method_name}=") { |v| store(key, v) }
     end
 
   end
