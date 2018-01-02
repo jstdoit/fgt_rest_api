bcd957445f851fb1af324ce97d2e586e7b04c404
Awesome Code <team@xinminlabs.com>
2018-01-02 09:46:17 +0000

    Auto corrected by following Lint/UnusedMethodArgument

--- lib/fgt_rest_api/fc_hash.rb
+++ lib/fgt_rest_api/fc_hash.rb
@@ -37,7 +37,7 @@ module FGT
       define_singleton_method(method_name) { fetch(key) }
     end
 
-    def setter_method(key, value)
+    def setter_method(key, _value)
       method_name = key.to_s.tr('-', '_') + '='
       return true if respond_to?(method_name.to_sym)
       define_singleton_method(method_name) { |v| store(key, v) }
